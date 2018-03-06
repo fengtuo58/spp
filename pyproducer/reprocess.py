6504600807e08fd5a908c2e623012b224aa28b7f
alexei.chijkov@gmail.com <alexei.chijkov@gmail.com>
2018-03-06 12:02:27 +0300

    ready tu use reprocess.py module

--- pyproducer/reprocess.py
+++ pyproducer/reprocess.py
@@ -5,45 +5,50 @@ from pykafka.common import CompressionType
 
 logging.basicConfig(level=logging.INFO)
 
-msg_count = 10 ** 6
-msg_payload = {"rpcookie": "45555", "data": [3333, 3344]}
-msg_len = len(bytes(json.dumps(msg_payload), 'utf-8'))
 
-print('Calculations based on:', json.dumps(msg_payload), ' length = ', msg_len)
-
-bootstrap_servers = 'localhost:9092'
-input_topic = b'test'
-output_topic = b'out'
-
-
-def get_consumer(use_rdkafka=False):
-    client = KafkaClient(hosts=bootstrap_servers)
-    topic = client.topics[input_topic]
-    consumer = topic.get_simple_consumer(use_rdkafka=use_rdkafka)
+def get_consumer(params):
+    client = KafkaClient(hosts=params['in_server_url'])
+    topic = client.topics[params['input_topic']]
+    consumer = topic.get_simple_consumer(use_rdkafka=params['use_rdkafka'])
     return consumer
 
 
-def get_producer(use_rdkafka=False):
-    client = KafkaClient(hosts=bootstrap_servers)
-    topic = client.topics[output_topic]
-    producer = topic.get_producer(use_rdkafka=use_rdkafka, compression=CompressionType.GZIP)
+def get_producer(params):
+    client = KafkaClient(hosts=params['out_server_url'])
+    topic = client.topics[params['output_topic']]
+    producer = topic.get_producer(use_rdkafka=params['use_rdkafka'], compression=CompressionType.GZIP)
     return producer
 
 
-consumer = get_consumer()
-producer = get_producer()
-msg_consumed_count = 0
+def process(params):
+    """
+        e.g params = dict(in_server_url="localhost:9092", out_server_url="localhost:9092",
+         use_rdkafka=False, input_topic=b'test', output_topic='out')
+    """
+    # set default values if not set before
+    params.setdefault('input_topic', b'test')
+    params.setdefault('output_topic', b'out')
+    params.setdefault('use_rdkafka', False)
 
-for message in consumer:
-    msg = consumer.consume()
-    if msg:
-        msg_consumed_count += 1
-        msg_string = msg.value.decode('utf8')
-        try:
-            msg_json = json.loads(msg_string)
-            if isinstance(msg_json, dict):
-                msg_json["repro"] = "success"
-                producer.produce(bytes(json.dumps(msg_json), 'utf-8'))
-                logging.info('reprocessed {} messages'.format(msg_consumed_count))
-        except json.decoder.JSONDecodeError as e:
-            logging.info('Skip incorrect message {}: {}'.format(msg_string, e))
+    consumer = get_consumer(params)
+    producer = get_producer(params)
+    msg_consumed_count = 0
+
+    for _ in consumer:
+        msg = consumer.consume()
+        if msg:
+            msg_consumed_count += 1
+            msg_string = msg.value.decode('utf8')
+            try:
+                msg_json = json.loads(msg_string)
+                if isinstance(msg_json, dict):
+                    msg_json["repro"] = "success"
+                    producer.produce(bytes(json.dumps(msg_json), 'utf-8'))
+                    logging.info('reprocessed {} messages'.format(msg_consumed_count))
+            except json.decoder.JSONDecodeError as e:
+                logging.info('Skip incorrect message {}: {}'.format(msg_string, e))
+
+
+if __name__ == "__main__":
+    # for testing purpose
+    process({'in_server_url': 'localhost:9092', 'out_server_url': 'localhost:9092'})
